978112f0d8dc5077646f1a0680373621d7d779b8
cainhimself <cainhimself@gmail.com>
2017-01-21 15:28:27 -0800

    Initial Commit

---
+++ CalcutipTests/CalcutipTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalcutipTests.swift
+//  CalcutipTests
+//
+//  Created by aRTIcHOkE on 1/21/17.
+//  Copyright © 2017 aRTIcHOkE. All rights reserved.
+//
+
+import XCTest
+@testable import Calcutip
+
+class CalcutipTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
